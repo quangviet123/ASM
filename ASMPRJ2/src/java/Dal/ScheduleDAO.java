136a92f522c4043ad905ca828fd5935e89547c11
quangviet123 <viettqhe186490@fpt.edu.vn>
2025-03-14 02:44:27 +0700

    Update ScheduleDAO.java

--- ASMPRJ2/src/java/Dal/ScheduleDAO.java
+++ ASMPRJ2/src/java/Dal/ScheduleDAO.java
@@ -33,12 +33,34 @@ public class ScheduleDAO extends DBContext{
         }
         return list;
     }
+     public List<ScheduleDTO> getSchedulebyManagerID( int EmployeeId ) {
+        List<ScheduleDTO> list = new ArrayList<>();
+        String sql = "select  sche.Id,sche.Date,sche.Status,e.Name from Schedule sche inner join Employee e on e.Id"
+                + " = sche.EmployeeId where e.Parentemployee = ?";
+        try {
+
+            PreparedStatement st = db.connection.prepareStatement(sql);
+            st.setInt(1,EmployeeId );
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                ScheduleDTO s = new ScheduleDTO();
+                s.setId(rs.getInt(1));
+                s.setDate(rs.getDate(2));
+                s.setStatus(rs.getBoolean(3));
+                s.seteName(rs.getString(4));
+                list.add(s);
+            }
+        } catch (SQLException ex) {
+        }
+        return list;
+    }
      public static void main(String[] args) {
         ScheduleDAO r=new ScheduleDAO();
-        List<ScheduleDTO> l=r.getSchedule();
+        List<ScheduleDTO> l=r.getSchedulebyManagerID(2);
         
         for (ScheduleDTO scheduleDTO : l) {
-            System.out.println(scheduleDTO.geteName());
+            System.out.println(scheduleDTO.getId()+" "+scheduleDTO.getDate());
         }
 }
 
