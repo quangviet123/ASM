50be2c705cdea0abd0276020ae73cd84d2222372
quangviet123 <viettqhe186490@fpt.edu.vn>
2025-03-16 04:47:54 +0700

    Create ViewAdmin
    Tạo xét duyệt đơn cho quản lý của Admin

---
+++ ASMPRJ2/src/java/Controller/RequestMNM.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Dal.RequestDAO;
+import Model.Account;
+import Model.RequestDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class RequestMNM extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RequestMNM</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RequestMNM at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int requestId = Integer.parseInt(request.getParameter("requestId"));
+        String action = request.getParameter("action");
+        RequestDAO dao = new RequestDAO();
+
+        if ("Approved".equalsIgnoreCase(action)) {
+            dao.UpdateStatusRequest("Aproved", requestId);
+        } else if ("Rejected".equalsIgnoreCase(action)) {
+            dao.UpdateStatusRequest("Rejected", requestId);
+        }
+         HttpSession session = request.getSession();
+         int managementId = (Integer) session.getAttribute("managementId");
+        List<RequestDTO> requestList = dao.getRequestbyManagerID(managementId);
+        request.setAttribute("requestList", requestList);
+        request.getRequestDispatcher("ViewAdmin.jsp").forward(request, response);
+    }
+
+    
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
